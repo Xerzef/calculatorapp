4c118ffcc551daff721113f368e08a20cea1de1f
Syed Meeran Ishrat Hussain <Hussain-SMI@ulster.ac.uk>
2024-10-01 10:05:13 +0100

    Added test line

--- calculatorapp/Program.cs
+++ calculatorapp/Program.cs
@@ -1,4 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Making a simple calculator app
 
-Console.WriteLine(("This is a test");
+Console.WriteLine("Type in the first Number followed by the enter key");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by the enter key");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+// result will be the addition of firstnum and secondnum
+
+int result = firstnumber + SecondNumber;
+
+Console.WriteLine("The result is " + result);
+Console.ReadKey();
+
